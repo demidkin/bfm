31715ad0aecef39991c0ae1afdd6cc4e70d13ea6
demidkin <demidkin.spb@gmail.com>
2018-05-15 14:10:13 +0300

    task1 all test ok

--- bfm4/Task1/test/VulnerableOneTest.js
+++ bfm4/Task1/test/VulnerableOneTest.js
@@ -8,9 +8,55 @@ const VulnerableOne = artifacts.require('VulnerableOne.sol');
 
 contract('VulnerableOne', function(accounts) {
 
-    it('test constructor', async function() {
+    it('constructor', async function() {
         const token1 = await VulnerableOne.new();
     });
+
+    it('add new user', async function() {
+        const token1 = await VulnerableOne.new();
+        await token1.add_new_user(accounts[1]);
+        await token1.add_new_user(accounts[2]);
+        await token1.add_new_user(accounts[3]);
+        await token1.add_new_user(accounts[4]);
+    });
+
+    it('remove user', async function() {
+        const token1 = await VulnerableOne.new();
+        await token1.add_new_user(accounts[1]);
+        await token1.add_new_user(accounts[2]);
+        await token1.add_new_user(accounts[3]);
+        await token1.add_new_user(accounts[4]);
+
+        await token1.remove_user(accounts[2]);
+    });
+
+    it('remove last user', async function() {
+        const token1 = await VulnerableOne.new();
+        await token1.add_new_user(accounts[1]);
+        await token1.add_new_user(accounts[2]);
+        await token1.add_new_user(accounts[3]);
+        await token1.add_new_user(accounts[4]);
+
+        await token1.remove_user(accounts[4]);
+    });
+    it('set super user', async function() {
+        const token1 = await VulnerableOne.new();
+        await token1.set_super_user(accounts[1]);
+    });
+    it('pay', async function() {
+        const token1 = await VulnerableOne.new();
+        await token1.pay();
+    });
+    it('withdraw', async function() {
+        const token1 = await VulnerableOne.new();
+        await token1.withdraw();
+    });
+    it('get_user_balance', async function() {
+        const token1 = await VulnerableOne.new();
+        await token1.get_user_balance(accounts[0]);
+    });
+
+
 
     
 
