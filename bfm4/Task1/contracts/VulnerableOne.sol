ecac6422d910bd885013cf3de1828663e4071e96
demidkin <demidkin.spb@gmail.com>
2018-05-15 13:25:13 +0300

    end teask1

--- bfm4/Task1/contracts/VulnerableOne.sol
+++ bfm4/Task1/contracts/VulnerableOne.sol
@@ -37,58 +37,65 @@ contract VulnerableOne {
 		set_super_user(msg.sender);
 		add_new_user(msg.sender);
 	}
-//Любой пользователь может сделать супер пользователя, нужна проверка onlySuperUser
-	function set_super_user(address _new_super_user) public {
+
+//1. Любой пользователь может сделать супер пользователя, добавим модификатор onlySuperUser
+//2. Наверно нужно добавить пользователя в список пользователей (users_list), если его еще там нет
+//3. ограничение списка пользователей до 255 (gaslimit)
+	function set_super_user(address _new_super_user) public onlySuperUser() {
+		require(users_list.length<256);
 		is_super_user[_new_super_user] = true;
-	}
-//вообще не понятно что это такое? msg.value
-	function pay() public payable {
-		require(users_map[msg.sender].created != 0);
-		users_map[msg.sender].ether_balance += msg.value;
-	}
-// не выполняеться событие добавления пользователя
-	function add_new_user(address _new_user) public onlySuperUser {
-		require(users_map[_new_user].created == 0);
-		users_map[_new_user] = UserInfo({ created: now, ether_balance: 0 });
-		users_list.push(_new_user);
-		emit UserAdded(msg.sender);
-	}
-	//атака по лимиту газа
-	//кто угодно может удалять полльзователей, добавим проверку на овнера
-	//может еще из суперпользователей удалять?
-	function remove_user(address _remove_user) public {
-		require(users_map[msg.sender].created != 0);
-		//поидеи просто сбросит к базовому значению
-		delete(users_map[_remove_user]);
-		bool shift = false;
-		for (uint i=0; i<users_list.length; i++) {
-			if (users_list[i] == _remove_user) {
-				shift = true;
-			}
-			//удаляемый просто заменяеться следующим пользователем
-			//нет проверки если удаляемый последний пользователь?
-			if (shift == true) {
-				users_list[i] = users_list[i+1];
-			}
-		}
-	}
-	// для тестов, разбираюсь с масивами
-	function getArray() public returns (uint){
-		address[] sta;
-		sta.push(1);
-		sta.push(2);
-		sta.push(3);
-		sta.push(4);
-		sta.push(5);
-		sta.push(6);
-		sta.push(7);
-		sta.push(8);
-		sta.push(9);
-		//sta.push(10);
-		return sta.length;
+		add_new_user(_new_super_user);
 	}
 
-	//Reentrancy Attacks
+//вообще не знаю что это такое? msg.value, добавил проверку msg.value > 0
+	function pay() public payable {
+		require(users_map[msg.sender].created != 0);
+		if (msg.value > 0) users_map[msg.sender].ether_balance += msg.value;
+	}
+
+//5. Не выполняеться событие добавления пользователя
+//6. + ограничение списка пользователей до 255 (gaslimit)
+	function add_new_user(address _new_user) public onlySuperUser() {
+		require(users_map[_new_user].created == 0);
+		require(users_list.length<256);
+		users_map[_new_user] = UserInfo({ created: now, ether_balance: 0 });
+		users_list.push(_new_user);
+		emit UserAdded(_new_user);
+	}
+//Возможна атака по лимиту газа, ограничили список пользователей до 255.
+//7. кто угодно может удалять полльзователей, добавим модификатор onlySuperUser
+//8. может еще из суперпользователей удалять?
+//9. Нет проверки если удаляемый последний пользователь в массиве
+//10. Не изменяеться дилина масива
+	function remove_user(address _remove_user) public onlySuperUser() {
+		require(users_map[msg.sender].created != 0);
+		delete(users_map[_remove_user]);
+		delete(is_super_user[_remove_user]);
+		if (users_list[users_list.length-1] == _remove_user){
+			delete users_list[users_list.length-1];
+        	users_list.length--;
+		} 
+		else 
+		{
+			bool shift = false;
+			for (uint i=0; i<users_list.length-1; i++) {
+				if (!shift && users_list[i] == _remove_user){
+					shift = true;
+				}
+			
+				if (shift == true) {
+					users_list[i] = users_list[i+1];
+				}
+			}
+			if (shift == true)
+			{
+				delete users_list[users_list.length-1];
+        		users_list.length--;
+			}	
+		}			
+	}
+
+	//11. Reentrancy Attacks
 	function withdraw() public {
 		uint256 balance = users_map[msg.sender].ether_balance;
 		users_map[msg.sender].ether_balance = 0;
